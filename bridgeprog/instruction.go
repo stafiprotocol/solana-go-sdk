26dea6113dfa2d03246df9931324c04fbc4b3b83
tpkeeper <tpkeeper.me@gmail.com>
2021-09-07 10:19:15 +0800

    add bridge prog

---
+++ bridgeprog/instruction.go
@@ -0,0 +1,193 @@
+package bridgeprog
+
+import (
+	"crypto/sha256"
+
+	"github.com/stafiprotocol/solana-go-sdk/common"
+	"github.com/stafiprotocol/solana-go-sdk/types"
+)
+
+type Instruction [8]byte
+
+var (
+	InstructionCreateBridge    Instruction
+	InstructionCreateProposal  Instruction
+	InstructionApprove         Instruction
+	InstructionSetOwners       Instruction
+	InstructionChangeThreshold Instruction
+)
+
+func init() {
+	createBridgeHash := sha256.Sum256([]byte("global:create_bridge"))
+	copy(InstructionCreateBridge[:], createBridgeHash[:8])
+	createProposalHash := sha256.Sum256([]byte("global:create_proposal"))
+	copy(InstructionCreateProposal[:], createProposalHash[:8])
+	approveHash := sha256.Sum256([]byte("global:approve"))
+	copy(InstructionApprove[:], approveHash[:8])
+	setOwnersHash := sha256.Sum256([]byte("global:set_owners"))
+	copy(InstructionSetOwners[:], setOwnersHash[:8])
+	changeThresholdHash := sha256.Sum256([]byte("global:change_threshold"))
+	copy(InstructionChangeThreshold[:], changeThresholdHash[:8])
+}
+
+func CreateBridge(
+	programID,
+	bridgeAccount common.PublicKey,
+	owners []common.PublicKey,
+	threshold uint64,
+	nonce uint8,
+	resourceIdToTokenProg map[[32]byte]common.PublicKey) types.Instruction {
+
+	data, err := common.SerializeData(struct {
+		Instruction           Instruction
+		Owners                []common.PublicKey
+		Threshold             uint64
+		Nonce                 uint8
+		ResourceIdToTokenProg map[[32]byte]common.PublicKey
+	}{
+		Instruction:           InstructionCreateBridge,
+		Owners:                owners,
+		Threshold:             threshold,
+		Nonce:                 nonce,
+		ResourceIdToTokenProg: resourceIdToTokenProg,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return types.Instruction{
+		ProgramID: programID,
+		Accounts: []types.AccountMeta{
+			{PubKey: bridgeAccount, IsSigner: false, IsWritable: true},
+			{PubKey: common.SysVarRentPubkey, IsSigner: false, IsWritable: false},
+		},
+		Data: data,
+	}
+}
+
+type ProposalUsedAccount struct {
+	Pubkey     common.PublicKey
+	IsSigner   bool
+	IsWritable bool
+}
+
+func CreateProposal(
+	programID common.PublicKey,
+	txUsedProgramID []common.PublicKey,
+	txUsedAccounts [][]types.AccountMeta,
+	txInstructionData [][]byte,
+	bridgeAccount common.PublicKey,
+	proposalAccount common.PublicKey,
+	ownerAccount common.PublicKey) types.Instruction {
+
+	data, err := common.SerializeData(struct {
+		Instruction       Instruction
+		TxUsedProgramID   []common.PublicKey
+		TxUsedAccounts    [][]types.AccountMeta
+		TxInstructionData [][]byte
+	}{
+		Instruction:       InstructionCreateProposal,
+		TxUsedProgramID:   txUsedProgramID,
+		TxUsedAccounts:    txUsedAccounts,
+		TxInstructionData: txInstructionData,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return types.Instruction{
+		ProgramID: programID,
+		Accounts: []types.AccountMeta{
+			{PubKey: bridgeAccount, IsSigner: false, IsWritable: false},
+			{PubKey: proposalAccount, IsSigner: false, IsWritable: true},
+			{PubKey: ownerAccount, IsSigner: true, IsWritable: false},
+			{PubKey: common.SysVarRentPubkey, IsSigner: false, IsWritable: false},
+		},
+		Data: data,
+	}
+}
+
+func Approve(
+	programID,
+	bridgeAccount,
+	multiSiner,
+	proposalAccount,
+	ownerAccount common.PublicKey,
+	remainingAccounts []types.AccountMeta) types.Instruction {
+
+	data, err := common.SerializeData(struct {
+		Instruction Instruction
+	}{
+		Instruction: InstructionApprove,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	accounts := []types.AccountMeta{
+		{PubKey: bridgeAccount, IsSigner: false, IsWritable: false},
+		{PubKey: multiSiner, IsSigner: false, IsWritable: false},
+		{PubKey: proposalAccount, IsSigner: false, IsWritable: true},
+		{PubKey: ownerAccount, IsSigner: true, IsWritable: false},
+	}
+
+	return types.Instruction{
+		ProgramID: programID,
+		Accounts:  append(accounts, remainingAccounts...),
+		Data:      data,
+	}
+}
+
+func ChangeThreshold(
+	programID,
+	bridgeAccount,
+	multiSiner common.PublicKey,
+	threshold uint64) types.Instruction {
+
+	data, err := common.SerializeData(struct {
+		Instruction Instruction
+		Threshold   uint64
+	}{
+		Instruction: InstructionChangeThreshold,
+		Threshold:   threshold,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	accounts := []types.AccountMeta{
+		{PubKey: bridgeAccount, IsSigner: false, IsWritable: true},
+		{PubKey: multiSiner, IsSigner: true, IsWritable: false},
+	}
+
+	return types.Instruction{
+		ProgramID: programID,
+		Accounts:  accounts,
+		Data:      data,
+	}
+}
+
+func GetRemainAccounts(ins []types.Instruction) []types.AccountMeta {
+	accountMetas := []types.AccountMeta{}
+	accountMap := make(map[string]types.AccountMeta)
+	for _, in := range ins {
+		accountMetas = append(accountMetas, types.AccountMeta{
+			PubKey:     in.ProgramID,
+			IsSigner:   false,
+			IsWritable: false,
+		})
+		accountMetas = append(accountMetas, in.Accounts...)
+	}
+	for i, _ := range accountMetas {
+		addrStr := accountMetas[i].PubKey.ToBase58()
+		accountMetas[i].IsWritable = accountMap[addrStr].IsWritable || accountMetas[i].IsWritable
+		accountMetas[i].IsSigner = false
+		accountMap[addrStr] = accountMetas[i]
+	}
+
+	ret := make([]types.AccountMeta, 0)
+	for _, value := range accountMap {
+		ret = append(ret, value)
+	}
+	return ret
+}
